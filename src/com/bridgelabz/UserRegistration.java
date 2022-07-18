7d37654f37e8b5e7a4af15b44ec99a73f6128a45
surajrajegore <rajegoresuraj2880@gmail.com>
2022-07-18 13:58:47 +0530

    Added:code to validate User Last Name

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -4,6 +4,7 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     final static String FIRST_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
+    final static String LAST_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}";
 
     public static void main(String[] args) {
         System.out.println("Welcome to User registration Using Regex");
@@ -11,6 +12,8 @@ public class UserRegistration {
         UserRegistration userRegistration = new UserRegistration();
         System.out.println("Enter First Name");
         userRegistration.isValidFirstName(UserInput.getInput());
+        System.out.println("Enter Last Name");
+        userRegistration.isValidLastName(UserInput.getInput());
     }
 
     private static boolean isValidInput(String regex, String userInput) {
@@ -25,9 +28,21 @@ public class UserRegistration {
 
         try {
             if (isValidInput(FIRST_NAME_REGEX, userInput)) {
-                System.out.println("Valid Name : " + userInput);
+                System.out.println("Valid First Name : " + userInput);
             } else {
-                throw new InvalidUserInformationException("Invalid Name : " + userInput);
+                throw new InvalidUserInformationException("Invalid First Name : " + userInput);
+            }
+        } catch (InvalidUserInformationException e) {
+            System.out.println(e);
+        }
+    }
+    private void isValidLastName(String userInput) {
+
+        try {
+            if (isValidInput(LAST_NAME_REGEX, userInput)) {
+                System.out.println("Valid Last Name : " + userInput);
+            } else {
+                throw new InvalidUserInformationException("Invalid Last Name : " + userInput);
             }
         } catch (InvalidUserInformationException e) {
             System.out.println(e);
