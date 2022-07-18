a2820f2249a4ee636fcdc043ca39efd009f7f585
surajrajegore <rajegoresuraj2880@gmail.com>
2022-07-18 18:53:51 +0530

    Added:code of validate password using rule3

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -7,7 +7,7 @@ public class UserRegistration {
     final static String LAST_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}";
     final static String EMAIL_REGEX = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
     final static String MOBILE_NO_REGEX = "(91)?\\s{1}?[6-9]{1}[0-9]{9}$";
-    final static String PASSWORD_REGEX_RULE_2 = "^(?=.*[A-Z])(?=.*[a-z]).{8,}$";
+    final static String PASSWORD_REGEX_RULE_3 = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}$";
 
     public static void main(String[] args) {
         System.out.println("Welcome to User registration Using Regex");
@@ -77,26 +77,26 @@ public class UserRegistration {
 
     }
 
-    private void isValidMobileNumber(String userInput){
-        try{
-            if (isValidInput(MOBILE_NO_REGEX, userInput)){
-                System.out.println("Valid Mobile Number:" +userInput);
-            }else {
-                throw new InvalidUserInformationException("Invalid Mobile Number " +userInput);
+    private void isValidMobileNumber(String userInput) {
+        try {
+            if (isValidInput(MOBILE_NO_REGEX, userInput)) {
+                System.out.println("Valid Mobile Number:" + userInput);
+            } else {
+                throw new InvalidUserInformationException("Invalid Mobile Number " + userInput);
             }
-        }catch (InvalidUserInformationException e){
+        } catch (InvalidUserInformationException e) {
             System.out.println(e);
         }
     }
 
-    private void isValidPassword(String userInput){
-        try{
-            if (isValidInput(PASSWORD_REGEX_RULE_2  , userInput)){
-                System.out.println("Valid Password:" +userInput);
-            }else {
-                throw new InvalidUserInformationException("Invalid Password " +userInput);
+    private void isValidPassword(String userInput) {
+        try {
+            if (isValidInput(PASSWORD_REGEX_RULE_3, userInput)) {
+                System.out.println("Valid Password:" + userInput);
+            } else {
+                throw new InvalidUserInformationException("Invalid Password " + userInput);
             }
-        }catch (InvalidUserInformationException e){
+        } catch (InvalidUserInformationException e) {
             System.out.println(e);
         }
     }
